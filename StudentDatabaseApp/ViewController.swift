22556879f71562f78fcad0caa5fa8bb386c155f5
Manuel Santos Souza <manuel.souza.017@ufrn.edu.br>
2025-02-23 17:38:09 -0300

    Showing the student names and adding the validators to the textForm

--- StudentDatabaseApp/ViewController.swift
+++ StudentDatabaseApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var studentNamesLabel: UILabel!
+	@IBOutlet weak var studentNameTextField: UITextField!
+	var students: [String] = []
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		studentNamesLabel.text = ""
+		studentNamesLabel.frame = CGRect(
+			x: 20,
+			y: 70,
+			width: view.frame.width - 40,
+			height: studentNamesLabel.frame.height * 3)
 	}
 
+	@IBAction func submitButtonTapped(_ sender: Any) {
+		guard let studentName = studentNameTextField.text,
+			studentName != "",
+			studentName.count > 3
+		else {
+			return
+		}
+
+		students.append(studentName)
+
+		var interingStudents = ""
+		for student in students {
+			interingStudents += " \(student)"
+		}
+
+		studentNamesLabel.text = interingStudents
+		studentNameTextField.text = ""
+	}
 
 }
-
